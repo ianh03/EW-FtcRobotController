a13b2146ee0777c7767d2b5c96367a09808ab52e
Ian H <ianhoang2006@hotmail.com>
2022-11-14 19:29:09 -0800

    Merge pull request #344 from FIRST-Tech-Challenge/20220907-131644-release-candidate
    FtcRobotController v8.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/ModAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autonomous/ModAutonomous.java
@@ -45,11 +45,33 @@ public class ModAutonomous extends LinearOpMode {
         }
         robot.stopMotors();
 
+        while (robot.getHeading() < 50) {
+            robot.tankLeft(0.35);
+        }
+        robot.stopMotors();
 
-        /*wait(750);
+        robot.move(100, 50, 0.7);
 
-        robot.move(275, -83, -0.7);
-         */
+        while (robot.getHeading() > -83) {
+            robot.tankRight(0.35);
+        }
+        robot.stopMotors();
+
+        while (robot.colorSensor.blue() < 500) {
+            robot.setPower(0.4,0.4,0.4,0.4);
+        }
+        robot.stopMotors();
+
+        robot.move(210, -83, 0.7);
+
+        wait(500);
+
+        robot.reverse(300, -85,-0.5);
+
+        while (robot.getHeading() > -83) {
+            robot.tankRight(0.35);
+        }
+        robot.stopMotors();
 
         stop();
     }
